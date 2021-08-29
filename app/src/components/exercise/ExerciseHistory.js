c0ca3a8b46e527738c69cc9e70920d930688ce93
Max Monis <mmonis77@gmail.com>
2021-08-29 09:35:45 -0400

    Allow toggle of x and y axis of table

--- app/src/components/exercise/ExerciseHistory.js
+++ app/src/components/exercise/ExerciseHistory.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import organizeRoutine from '../../functions/organizeRoutine';
 import { formatDate } from '../../functions/helpers';
+import useToggle from '../../hooks/useToggle';
 
 const ExerciseHistory = ({ workouts }) => {
   const lifts = {};
@@ -14,33 +15,65 @@ const ExerciseHistory = ({ workouts }) => {
     liftArray.push({ lift, total: lifts[lift] });
   }
   const sortedLifts = liftArray.sort((a, b) => b.total - a.total);
+  const [sortByDate, toggleSort] = useToggle(true);
   return (
-    <div className='workout-table'>
-      <h2>Exercise History</h2>
+    <div className='exercise-history'>
       <div>
-        {[{}, ...workouts].map(({ routine, date, id }) => (
-          <div key={id || 'id'}>
-            <div className='workout-table-head'>
-              {date ? <h3>{formatDate(date)}</h3> : null}
-            </div>
-            {sortedLifts.map(({ lift }) => (
-              <div className='workout-table-item' key={lift}>
-                {id ? (
-                  organizeRoutine(
-                    routine.filter(exercise => exercise.lift === lift)
-                  ).map(exercise => (
-                    <h3 key={exercise.id} title={lift}>
-                      {exercise.printout}
-                    </h3>
-                  ))
-                ) : (
-                  <h3>{lift}</h3>
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
+        <h2 className='pointer' onClick={toggleSort}>
+          Exercise History
+        </h2>
       </div>
+      {sortByDate ? (
+        <div>
+          {[{}, ...workouts].map(({ routine, date, id }) => (
+            <div key={id || 'id'}>
+              <div className='exercise-history-head'>
+                {date ? <h3>{formatDate(date)}</h3> : null}
+              </div>
+              {sortedLifts.map(({ lift }) => (
+                <div className='exercise-history-item' key={lift}>
+                  {id ? (
+                    organizeRoutine(
+                      routine.filter(exercise => exercise.lift === lift)
+                    ).map(exercise => (
+                      <h3 key={exercise.id} title={lift}>
+                        {exercise.printout}
+                      </h3>
+                    ))
+                  ) : (
+                    <h3>{lift}</h3>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          {[{}, ...sortedLifts].map(({ lift }) => (
+            <div key={lift || 'id'}>
+              <div className='exercise-history-head'>
+                {lift ? <h3>{lift}</h3> : null}
+              </div>
+              {workouts.map(({ routine, date, name, id }) => (
+                <div className='exercise-history-item' key={id}>
+                  {lift ? (
+                    organizeRoutine(
+                      routine.filter(exercise => exercise.lift === lift)
+                    ).map(exercise => (
+                      <h3 key={exercise.id} title={lift}>
+                        {exercise.printout}
+                      </h3>
+                    ))
+                  ) : (
+                    <h3 title={name}>{formatDate(date)}</h3>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
