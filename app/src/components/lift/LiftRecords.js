9223c54ba2007a87cf3cffef1e91a38b12300cb8
Max Monis <mmonis77@gmail.com>
2021-08-30 22:37:57 -0400

    Update horizontal scrolling of lift records

--- app/src/components/lift/LiftRecords.js
+++ app/src/components/lift/LiftRecords.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { formatDate } from '../../functions/helpers';
 
 const LiftRecords = ({ records, lift, setExercise }) => {
@@ -7,20 +7,46 @@ const LiftRecords = ({ records, lift, setExercise }) => {
     surpassed
       ? `${formatDate(becameRecord)} - ${formatDate(surpassed)}`
       : formatDate(becameRecord);
+  const [horizontalIndex, setHorizontalIndex] = useState(2);
+  const increment = () =>
+    horizontalIndex < liftRecords.length &&
+    setHorizontalIndex(horizontalIndex + 2);
+  const decrement = () => setHorizontalIndex(horizontalIndex - 2 || 2);
+  useEffect(() => {
+    setHorizontalIndex(2);
+  }, [lift]);
   return (
     <div className='lift-records'>
       {!liftRecords.length ? (
         <h4>{lift} records will be displayed here</h4>
       ) : (
         <>
-          {liftRecords.map(exercise => (
-            <button
-              key={exercise.id}
-              onClick={() => setExercise(exercise)}
-              title={getTitle(exercise)}>
-              {exercise.printout}
-            </button>
-          ))}
+          <button
+            className={horizontalIndex > 2 ? '' : 'opacity-0 cursor-default'}
+            onClick={decrement}>
+            {'<-'}
+          </button>
+          <div>
+            {liftRecords
+              .slice(horizontalIndex - 2, horizontalIndex)
+              .map(exercise => (
+                <button
+                  key={exercise.id}
+                  onClick={() => setExercise(exercise)}
+                  title={getTitle(exercise)}>
+                  {exercise.printout}
+                </button>
+              ))}
+          </div>
+          <button
+            className={
+              horizontalIndex < liftRecords.length
+                ? ''
+                : 'opacity-0 cursor-default'
+            }
+            onClick={increment}>
+            {'->'}
+          </button>
         </>
       )}
     </div>
