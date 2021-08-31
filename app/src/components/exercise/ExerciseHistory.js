b3e3f2af5c00d7b2dfdf16643ce1f9ff6ea6fdf3
Max Monis <mmonis77@gmail.com>
2021-08-30 22:41:53 -0400

    Update scrolling of exercise history

--- app/src/components/exercise/ExerciseHistory.js
+++ app/src/components/exercise/ExerciseHistory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import organizeRoutine from '../../functions/organizeRoutine';
 import { formatDate } from '../../functions/helpers';
 import useToggle from '../../hooks/useToggle';
@@ -15,18 +15,30 @@ const ExerciseHistory = ({ workouts }) => {
     liftArray.push({ lift, total: lifts[lift] });
   }
   const sortedLifts = liftArray.sort((a, b) => b.total - a.total);
-  const [sortByDate, toggleSort] = useToggle(false);
+  const [sortByDate, toggleSort] = useToggle(true);
+  const [horizontalIndex, setHorizontalIndex] = useState(1);
+  const increment = () => setHorizontalIndex(horizontalIndex + 1);
+  const decrement = () => setHorizontalIndex(horizontalIndex - 1 || 1);
+  const [displayedDates, setDisplayedDates] = useState(10);
   return (
     <div className='exercise-history'>
       <div>
+        <button
+          className={horizontalIndex > 1 ? '' : 'opacity-0 cursor-default'}
+          onClick={decrement}>
+          {'<-'}
+        </button>
         <h2 className='pointer' onClick={toggleSort}>
           Exercise History
         </h2>
+        <button onClick={increment}>{'->'}</button>
       </div>
       {sortByDate ? (
         <div>
-          {[{}, ...workouts].map(({ routine, date, id }) => (
-            <div key={id || 'id'}>
+          {[{}, ...workouts].map(({ routine, date, id }, i) => (
+            <div
+              className={i && i < horizontalIndex ? 'display-none' : ''}
+              key={id || 'id'}>
               <div className='exercise-history-head'>
                 {date ? <h3>{formatDate(date)}</h3> : null}
               </div>
@@ -49,30 +61,43 @@ const ExerciseHistory = ({ workouts }) => {
           ))}
         </div>
       ) : (
-        <div>
-          {[{}, ...sortedLifts].map(({ lift }) => (
-            <div key={lift || 'id'}>
-              <div className='exercise-history-head'>
-                {lift ? <h3>{lift}</h3> : null}
-              </div>
-              {workouts.map(({ routine, date, name, id }) => (
-                <div className='exercise-history-item' key={id}>
-                  {lift ? (
-                    organizeRoutine(
-                      routine.filter(exercise => exercise.lift === lift)
-                    ).map(exercise => (
-                      <h3 key={exercise.id} title={lift}>
-                        {exercise.printout}
-                      </h3>
-                    ))
-                  ) : (
-                    <h3 title={name}>{formatDate(date)}</h3>
-                  )}
+        <>
+          <div>
+            {[{}, ...sortedLifts].map(({ lift }, i) => (
+              <div
+                className={i && i < horizontalIndex ? 'display-none' : ''}
+                key={lift || 'id'}>
+                <div className='exercise-history-head'>
+                  {lift ? <h3>{lift}</h3> : null}
                 </div>
-              ))}
+                {workouts
+                  .slice(0, displayedDates)
+                  .map(({ routine, date, name, id }) => (
+                    <div className='exercise-history-item' key={id}>
+                      {lift ? (
+                        organizeRoutine(
+                          routine.filter(exercise => exercise.lift === lift)
+                        ).map(exercise => (
+                          <h3 key={exercise.id} title={lift}>
+                            {exercise.printout}
+                          </h3>
+                        ))
+                      ) : (
+                        <h3 title={name}>{formatDate(date)}</h3>
+                      )}
+                    </div>
+                  ))}
+              </div>
+            ))}
+          </div>
+          {workouts.length > displayedDates && (
+            <div>
+              <button className='outline m-20' onClick={() => setDisplayedDates(displayedDates + 10)}>
+                More Workouts
+              </button>
             </div>
-          ))}
-        </div>
+          )}
+        </>
       )}
     </div>
   );
