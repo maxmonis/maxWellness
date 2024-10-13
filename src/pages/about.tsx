9e06b616312bccf14d95517b474f41ec6a4c8ca7
Max Monis <mmonis77@gmail.com>
2024-10-12 22:17:30 -0400

    Minor updates to about page styles

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -22,8 +22,8 @@ export default function InfoPage() {
 					<div className="flex flex-col gap-12 pb-12 pt-6">
 						<div>
 							<div className="mx-auto flex max-w-prose flex-col gap-4">
-								<h3 className="text-center text-lg">Overview</h3>
-								<p>
+								<h3 className="text-center font-bold">Overview</h3>
+								<p className="text-sm">
 									This website allows you to create a free account using your
 									Google credentials (recommended) or an email/password
 									combination. It&apos;s a handy way to keep track of your
@@ -35,8 +35,8 @@ export default function InfoPage() {
 						</div>
 						<div>
 							<div className="mx-auto flex max-w-prose flex-col gap-4">
-								<h3 className="text-center text-lg">Creating Workouts</h3>
-								<p>
+								<h3 className="text-center font-bold">Creating Workouts</h3>
+								<p className="text-sm">
 									To create a new workout, add exercises by selecting their
 									name, sets, reps, and weight. You can drag and drop exercises
 									to reorder them, and clicking the x icon next to an exercise
@@ -45,19 +45,19 @@ export default function InfoPage() {
 									button.
 								</p>
 								<Video src="creating-workouts" />
-								<p>
+								<p className="text-sm">
 									Each exercise must include a weight or at least one rep. This
 									is because you could do 10 bodyweight squats or you could
 									squat 100 pounds one time, but you can&apos;t do zero reps
 									with zero weight.
 								</p>
-								<p>
+								<p className="text-sm">
 									Back-to-back sets of the same exercise with the same weight
 									and reps will automatically be combined. For example,
 									2(10x100) immediately followed by 3(10x100) will become
 									5(10x100).
 								</p>
-								<p>
+								<p className="text-sm">
 									Personal records will be indicated with asterisks (one
 									indicates that the record has been broken, two means it&apos;s
 									still your record). These records will be automatically
@@ -67,8 +67,8 @@ export default function InfoPage() {
 						</div>
 						<div>
 							<div className="mx-auto flex max-w-prose flex-col gap-4">
-								<h3 className="text-center text-lg">Managing Names</h3>
-								<p>
+								<h3 className="text-center font-bold">Managing Names</h3>
+								<p className="text-sm">
 									The Settings page allows you to add, update, or delete the
 									names you use for workouts and exercises. Names must be unique
 									and can be deleted unless they&apos;re currently included in
@@ -81,8 +81,8 @@ export default function InfoPage() {
 						</div>
 						<div>
 							<div className="mx-auto flex max-w-prose flex-col gap-4">
-								<h3 className="text-center text-lg">Managing Workouts</h3>
-								<p>
+								<h3 className="text-center font-bold">Managing Workouts</h3>
+								<p className="text-sm">
 									The ellipsis icon to the right of each workout in the list
 									allows you to duplicate it, copy it to clipboard, edit it, or
 									delete it. Please note that edits and deletions cannot be
@@ -93,8 +93,8 @@ export default function InfoPage() {
 						</div>
 						<div>
 							<div className="mx-auto flex max-w-prose flex-col gap-4">
-								<h3 className="text-center text-lg">Tips and Tricks</h3>
-								<p>
+								<h3 className="text-center font-bold">Tips and Tricks</h3>
+								<p className="text-sm">
 									While you&apos;re entering a new workout you can click on the
 									name, date, or exercise of an existing workout to copy that
 									value. The Filters page allows you to sort or filter the
@@ -102,7 +102,7 @@ export default function InfoPage() {
 									to view your workouts sorted by date or exercise name.
 								</p>
 								<Video src="tips-and-tricks" />
-								<p>
+								<p className="text-sm">
 									If you ever need help you can always return to this About page
 									by clicking the question mark icon in the navbar. That&apos;s
 									all you need to know, time to add some workouts!
