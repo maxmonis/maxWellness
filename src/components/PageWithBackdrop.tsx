c47714ce8d0b10a954d8d84ab35e22f598392765
Max Monis <mmonis77@gmail.com>
2024-10-14 18:03:36 -0400

    Add title to page with backdrop

--- src/components/PageWithBackdrop.tsx
+++ src/components/PageWithBackdrop.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Image from "next/image"
 import * as React from "react"
 
@@ -6,37 +7,42 @@ export function PageWithBackdrop({
 	title,
 }: React.PropsWithChildren<{ title: string }>) {
 	return (
-		<div className="dark fixed left-0 top-0 -z-10 bg-[url('/barbell.jpg')] bg-cover bg-[30%] bg-no-repeat object-cover">
-			<div className="relative h-screen w-screen overflow-auto px-4 pb-2 pt-10">
-				<div className="flex h-full flex-col items-center justify-between gap-20">
-					<div className="w-full max-w-xs rounded-lg border bg-background px-4 pb-6 pt-4 text-foreground xs:px-6">
-						<div>
-							<div className="mb-4 flex w-full justify-center">
-								<span className="flex items-center gap-2 text-lg">
-									<Image
-										alt="maxWellness logo"
-										className="h-6 w-6"
-										height={24}
-										src="/android-chrome-192x192.png"
-										width={24}
-									/>
-									maxWellness
-								</span>
+		<>
+			<Head>
+				<title>{`maxWellness | ${title}`}</title>
+			</Head>
+			<div className="dark fixed left-0 top-0 -z-10 bg-[url('/barbell.jpg')] bg-cover bg-[30%] bg-no-repeat object-cover">
+				<div className="relative h-screen w-screen overflow-auto px-4 pb-2 pt-10">
+					<div className="flex h-full flex-col items-center justify-between gap-20">
+						<div className="w-full max-w-xs rounded-lg border bg-background px-4 pb-6 pt-4 text-foreground xs:px-6">
+							<div>
+								<div className="mb-4 flex w-full justify-center">
+									<span className="flex items-center gap-2 text-lg">
+										<Image
+											alt="maxWellness logo"
+											className="h-6 w-6"
+											height={24}
+											src="/android-chrome-192x192.png"
+											width={24}
+										/>
+										maxWellness
+									</span>
+								</div>
+								<h1 className="mb-2 font-bold">{title}</h1>
 							</div>
-							<h1 className="mb-2 font-bold">{title}</h1>
+							{children}
 						</div>
-						{children}
+						<a
+							className="text-sm text-foreground"
+							href="https://maxmonis.com"
+							rel="noreferrer"
+							target="_blank"
+						>
+							© Max Monis 2019-{new Date().getFullYear()}
+						</a>
 					</div>
-					<a
-						className="text-sm text-foreground"
-						href="https://maxmonis.com"
-						rel="noreferrer"
-						target="_blank"
-					>
-						© Max Monis 2019-{new Date().getFullYear()}
-					</a>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
