fbdceae2b5819da608a23364c1a077083f3beb82
Max Monis <mmonis77@gmail.com>
2024-10-20 09:52:28 -0400

    Simplify settings loading state

--- src/features/settings/components/SettingsLoader.tsx
+++ src/features/settings/components/SettingsLoader.tsx
@@ -1,53 +1,21 @@
 import { BackButton } from "@/components/BackButton"
-import { cn } from "@/lib/utils"
 
 /**
  * Displays a loading skeleton for the /settings route
  */
 export function SettingsLoader() {
 	return (
-		<div className="flex min-h-screen flex-col justify-between lg:max-w-3xl lg:border-r">
+		<div className="min-h-screen lg:max-w-3xl lg:border-r">
 			<div className="flex h-14 items-end border-b px-4 pb-2 lg:px-6">
 				<BackButton />
 				<h1 className="text-lg">Settings</h1>
 			</div>
 			<div className="flex w-full flex-grow justify-center">
-				<div className="flex max-h-[calc(100dvh-7rem)] flex-grow divide-x md:max-h-[calc(100dvh-3.5rem)]">
-					{Array.from({ length: 2 }).map((_, i) => (
-						<div
-							className="flex w-full flex-grow flex-col items-center overflow-hidden"
-							key={i}
-						>
-							<div className="flex w-full flex-grow flex-col justify-center overflow-hidden px-4 pt-4 lg:px-6">
-								<h2 className="mx-auto mb-4 text-center font-bold">
-									{i ? "Workouts" : "Exercises"}
-								</h2>
-								<div className="flex">
-									<span className="h-7 w-full animate-pulse rounded bg-slate-300 dark:bg-slate-700" />
-								</div>
-								<div className="flex h-full flex-col gap-5 pt-6">
-									{Array.from({ length: 3 - i }).map((_, j) => (
-										<div className="flex flex-col gap-4" key={`${i}-${j}`}>
-											{Array.from({ length: 2 }).map((_, k) => (
-												<div
-													className="flex items-center justify-between"
-													key={`${i}-${j}-${k}`}
-												>
-													<span
-														className={cn(
-															"ml-1 h-3 animate-pulse rounded bg-slate-300 dark:bg-slate-700",
-															k ? "w-16" : "w-20",
-															((i + j) * k) % 5 && "max-xs:h-5 xs:w-28",
-														)}
-													/>
-												</div>
-											))}
-										</div>
-									))}
-								</div>
-							</div>
-						</div>
-					))}
+				<div className="mx-auto flex h-full max-h-[calc(100dvh-7rem)] w-full justify-center border-t md:max-h-[calc(100dvh-3.5rem)]">
+					<span
+						aria-busy="true"
+						className="mt-20 h-20 w-20 animate-spin rounded-full border-4 border-r-transparent"
+					/>
 				</div>
 			</div>
 		</div>
